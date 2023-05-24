5614a3a6631b4fa90538bda302ee58db3fe37a90
Satan Sharma <satansharma7549@gmail.com>
2023-05-25 02:54:54 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -3,56 +3,68 @@ const userModel = require("../models/userModels");
 const bcrypt = require("bcrypt");
 const sendToken = require("../utils/jswToken");
 
-// register User;
+// Register User
 const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
-
-  // Check if user already exists
+  console.log(name, email, password);
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Please fill the Credentials",
+    });
+  }
+  // Hash the password
   const hashedPassword = await bcrypt.hash(password, 5);
 
+  // Create a new user
   const user = await userModel.create({
     name,
     email,
     password: hashedPassword,
   });
 
+  // Send token in the response
   sendToken(res, 201, user);
 });
 
-//Login User
+// Login User
 const userLogin = catchAsyncErrors(async (req, res, next) => {
   const { email, password } = req.body;
-
+  console.log(email, password);
   if (!email || !password) {
     return res.status(400).json({
-      success: true,
-      message: "Please fill the Email and Password Both",
+      success: false,
+      message: "Please fill in both Email and Password",
     });
   }
 
-  //select useing because by default password has select: false
+  // Find the user by email
   const userExists = await userModel.findOne({ email });
 
   if (!userExists) {
     return res.status(401).json({
+      success: false,
       message: "Invalid Email or Password",
     });
   }
 
-  //password compare
+  // Compare the provided password with the stored password
   const isPassword = await userExists.comparePassword(password);
 
   if (!isPassword) {
     return res.status(401).json({
+      success: false,
       message: "Invalid Email or Password",
     });
   }
 
+  // Send token in the response
   sendToken(res, 200, userExists);
 });
 
-//Logout User
+// Logout User
 const logoutUser = catchAsyncErrors(async (req, res, next) => {
+  // Clear the token cookie
   res.cookie("token", null, {
     expires: new Date(Date.now()),
     httpOnly: true,
@@ -64,8 +76,12 @@ const logoutUser = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+// Get All Users
 const getAllUsers = catchAsyncErrors((req, res, next) => {
-  res.send("All user");
+  res.status(200).json({
+    success: true,
+    message: "All user",
+  });
 });
 
 module.exports = {
