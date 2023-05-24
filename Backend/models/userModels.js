ef68b82e3940d48019189e8f513f7586b0ea121b
Satan Sharma <satansharma7549@gmail.com>
2023-05-25 02:55:08 +0530

    Update userModels.js

--- Backend/models/userModels.js
+++ Backend/models/userModels.js
@@ -3,24 +3,24 @@ const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+// Define the user schema
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "please enter  your name"],
-    maxLength: [50, "name can't exced 50 characters"],
-    minLength: [3, "name should have more than 3 character"],
+    required: [true, "please enter your name"],
+    maxLength: [50, "name can't exceed 50 characters"],
+    minLength: [3, "name should have more than 3 characters"],
   },
   email: {
     type: String,
     required: [true, "please enter email"],
     unique: true,
-    validate: [validator.isEmail, "please enter valid email"],
+    validate: [validator.isEmail, "please enter a valid email"],
   },
   password: {
     type: String,
-    required: [true, "please enter password"],
-    minLength: [6, "password should be greater than 6 characters"],
-    // select: false,
+    required: [true, "please enter a password"],
+    minLength: [3, "password should be greater than 3 characters"],
   },
   createdAt: {
     type: Date,
@@ -28,18 +28,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// JWT Token generate method of userSchema
+// Method to generate a JWT token for the user
 userSchema.methods.getjwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_SECRET_KEY_EXPIRE,
   });
 };
 
-// compare password
+// Method to compare the user's password with a provided password
 userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+// Create the User model
 const UserModel = mongoose.model("user", userSchema);
 
 module.exports = UserModel;
