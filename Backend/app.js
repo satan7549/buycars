e8ccd055ed17c19c0d108d3cb1d70ab8b818166b
Satan Sharma <satansharma7549@gmail.com>
2023-05-24 18:49:39 +0530

    Server Set-up and successfully run

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+/* import all routes */
+
+
+
+
+module.exports = app;
