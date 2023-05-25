1f923f3b0f6001d7431503f991e945cee0e90ca0
Satan Sharma <satansharma7549@gmail.com>
2023-05-26 02:26:41 +0530

    Cloudinary config for product image upload

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const app = require("./app");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config/config.env" });
 const ConnectDB = require("./config/db.connect");
+const cloudinary = require("cloudinary");
 
 /* App connects to the database */
 
@@ -14,6 +15,12 @@ process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting down the server due to uncaught exception`);
   process.exit(1);
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Starting the server and connecting to the database
