61e475038f043c2df80b2e846d3ec1b6b9330a9b
Satan Sharma <satansharma7549@gmail.com>
2023-05-26 02:26:48 +0530

    Update inventory.controller.js

--- Backend/controllers/inventory.controller.js
+++ Backend/controllers/inventory.controller.js
@@ -1,9 +1,33 @@
 const catchAsyncErrors = require("../middleware/catchAsyncError");
 const InventoryModel = require("../models/inventoryModels");
+const cloudinary = require("cloudinary");
 
 // Function to add a new marketplace inventory entry
 const addMPInventory = catchAsyncErrors(async (req, res, next) => {
-  req.body.user = req.user.id;
+  let images = [];
+
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+
+  const imagesLinks = [];
+
+  for (let i = 0; i < images.length; i++) {
+    const result = await cloudinary.v2.uploader.upload(images[i], {
+      folder: "cars",
+    });
+
+    imagesLinks.push({
+      public_id: result.public_id,
+      url: result.secure_url,
+    });
+  }
+
+  req.body.images = imagesLinks;
+  req.body.user = req.user._id;
+  console.log(req.body);
   const newEntry = new InventoryModel(req.body);
   await newEntry.save();
   res.status(200).json({ message: "addedd successs", newEntry });
