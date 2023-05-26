6774d53d549861e5f348974f6501ea6287044f00
Satan Sharma <satansharma7549@gmail.com>
2023-05-26 19:13:47 +0530

    Update CarDetail.jsx

--- frontend/src/Pages/CarDetail.jsx
+++ frontend/src/Pages/CarDetail.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getSingleInventoryItems } from "../Redux/Inventory/inventory.action";
+import { Box, Image, Text } from "@chakra-ui/react";
 
 const CarDetail = () => {
-  return <div>Car Details</div>;
+  const { id } = useParams();
+  const { singleItem } = useSelector((store) => store.inventory);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSingleInventoryItems(id));
+  }, [id, dispatch]);
+
+  console.log("cardetail page",singleItem);
+  return (
+    <Box mt={"200px"}>
+      <Box>
+        <Text>{singleItem.model}</Text>
+        <Text>{singleItem.year}</Text>
+        <Text>{singleItem.kmOnOdometer}</Text>
+        <Box>
+          {singleItem.images?.map((image) => (
+            <Image
+              width={"100px"}
+              height={"100px"}
+              key={image._id}
+              src={image.url}
+            />
+          ))}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default CarDetail;
