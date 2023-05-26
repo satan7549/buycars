8d3436ec606d5733ac3f8dc2f3136272d6f863a0
Satan Sharma <satansharma7549@gmail.com>
2023-05-26 19:14:10 +0530

    Create inventory.actionType.js

---
+++ frontend/src/Redux/Inventory/inventory.actionType.js
@@ -0,0 +1,33 @@
+// get All inventry items
+export const GET_INVENTORY_ITEM_LOADING = "inventory/items/get/laoding";
+export const GET_INVENTORY_ITEM_SUCCESS = "inventory/items/get/success";
+export const GET_INVENTORY_ITEM_ERROR = "inventory/items/get/error";
+
+// get All inventry items
+export const GET_SINGLE_INVENTORY_ITEM_LOADING = "single/inventory/items/get/laoding";
+export const GET_SINGLE_INVENTORY_ITEM_SUCCESS = "single/inventory/items/get/success";
+export const GET_SINGLE_INVENTORY_ITEM_ERROR = "single/inventory/items/get/error";
+
+
+
+// add item to inventory
+export const ADD_ITEM_TO_INVENTORY_LOADING = "iNVENTORY/items/add/laoding";
+export const ADD_ITEM_TO_INVENTORY_SUCCESS = "iNVENTORY/items/add/success";
+export const ADD_ITEM_TO_INVENTORY_ERROR = "inventory/items/add/error";
+
+// update inventory items
+export const UPDATE_INVENTORY_ITEM_LOADING = "inventory/items/update/laoding";
+export const UPDATE_INVENTORY_ITEM_SUCCESS = "inventory/items/update/success";
+export const UPDATE_INVENTORY_ITEM_ERROR = "inventory/items/update/error";
+
+// REMOVE inventory items
+export const REMOVE_INVENTORY_ITEM_LOADING = "inventory/items/remove/laoding";
+export const REMOVE_INVENTORY_ITEM_SUCCESS = "inventory/items/remove/success";
+export const REMOVE_INVENTORY_ITEM_ERROR = "inventory/items/remove/error";
+
+// Reset
+export const RESET_INVENTORY_ITEM = "inventory/items/reset";
+
+
+
+
