90adfb2b5feb3d0709fcdd446954d21120cae306
Satan Sharma <satansharma7549@gmail.com>
2023-05-26 19:13:16 +0530

    Update inventory.route.js

--- Backend/routes/inventory.route.js
+++ Backend/routes/inventory.route.js
@@ -3,13 +3,19 @@ const {
   getAllMPInventory,
   getUserMPInventry,
   addMPInventory,
+  getSingleMPInventory,
+  deleteInventory,
+  updateInventoryItem,
 } = require("../controllers/inventory.controller");
 const { isUserAuthenticated } = require("../middleware/userAuth");
 
 const inventoryRoutes = express.Router();
 
 inventoryRoutes.get("/", getAllMPInventory);
+inventoryRoutes.get("/:id", getSingleMPInventory);
 inventoryRoutes.get("/unique", isUserAuthenticated, getUserMPInventry);
 inventoryRoutes.post("/add", isUserAuthenticated, addMPInventory);
+inventoryRoutes.patch("/update/:id", isUserAuthenticated, updateInventoryItem);
+inventoryRoutes.delete("/delete/:id", isUserAuthenticated, deleteInventory);
 
 module.exports = inventoryRoutes;
